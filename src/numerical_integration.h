2a1954522c677ed3adeae2ded9bf802337661c4a
timmerj1 <timmerman.jeroen@gmail.com>
2025-02-28 18:26:24 +0100

    Integration hack changed to only be used with err_code 1

--- src/numerical_integration.h
+++ src/numerical_integration.h
@@ -56,7 +56,7 @@ NumericVector f_integrate(Rcpp::NumericMatrix pars,
   double err_est;
   int err_code;
   double res = integrate(f, lower, upper, err_est, err_code);
-  if (err_code != 0 && upper == R_PosInf) {
+  if (err_code == 1 && upper == R_PosInf) {
     double err_est_hacky;
     int err_code_hacky;
     double res_hacky = integrate(f, lower, 10, err_est_hacky, err_code_hacky);
